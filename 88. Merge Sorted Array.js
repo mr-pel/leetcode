9f77cef50247f34934407f6b766ed1046df4fe3e
dmitriipalii <json.ninja@yandex.com>
2024-10-19 13:27:15 +0300

    merge

--- 88. Merge Sorted Array.js
+++ 88. Merge Sorted Array.js
@@ -5,7 +5,7 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
     for (let p1 = m - 1, p2 = n - 1, p = m + n - 1; p >= 0; p--) {
         if (p2 < 0 || (p1 >= 0 && nums1[p1] > nums2[p2])) {
             nums1[p] = nums1[p1--];
@@ -13,7 +13,7 @@ var merge = function(nums1, m, nums2, n) {
             nums1[p] = nums2[p2--];
         }
     }
-    return nums1
+    return nums1;
 };
 
-console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
